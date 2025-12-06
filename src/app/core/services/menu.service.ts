d1ccc1b76ce94d65aa5ed32a4a5bd04e7930bdfd
Drey012 <drey01214@gmail.com>
2025-12-06 10:01:41 -0300

    fix: production link updated

--- src/app/core/services/menu.service.ts
+++ src/app/core/services/menu.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { MenuItem, ApiResponse } from '../../shared/models/menu-item.model';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 
 /**
  * Serviço para consumir a API do Cardápio
